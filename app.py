2860963df57814450b1f776889956ea9c82d6bf1
tiaditaa <118504168+tiaditaa@users.noreply.github.com>
2024-11-22 14:26:34 +0700

    p

--- app.py
+++ app.py
@@ -179,7 +179,6 @@ def emoji_detection():
 
     return render_template("emoji-detection.html", predictions=predictions)
 
-<<<<<<< HEAD
 
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
@@ -236,7 +235,5 @@ def video_stream():
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-=======
->>>>>>> 7a1080a1ffdf6a77ff87cba5540a9c9aef3c897a
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
